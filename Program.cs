350d5266950972aab5f2553bb02a64807c788830
matheus678 <60980352+matheus678@users.noreply.github.com>
2020-06-05 22:31:01 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace NumeralExtenso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá usuário, irei lhe ajudar a converter seu número para extenso");
+            Console.WriteLine("Pressione uma tecla para continuar...");
+            Console.ReadKey();
+
+             int Extenso = Convert.ToInt32(Console.ReadLine());
+
+            // int quer dizer que estaremos trabalhando com um número inteiro
+            // Extenso é a variável escolhida 
+
+            Console.Write("Digite o número a ser escrito por extenso...");
+
+            Console.WriteLine($"Seu número por extenso é igual a: {Extenso.ToWords()}.");
+
+
+
+
+        }
+    }
+}
